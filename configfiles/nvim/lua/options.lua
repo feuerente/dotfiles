032f0589f52b32a175f9983172b4dd1f5cc99240
Ralf Herp <65463376+feuerente@users.noreply.github.com>
2024-03-22 22:53:01 +0100

    [nvim] Add config based on modular kickstart.nvim

---
+++ configfiles/nvim/lua/options.lua
@@ -0,0 +1,50 @@
+-- [[ Setting options ]]
+-- :help vim.opt
+-- :help option-list
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- Enable mouse mode, can be useful for resizing splits for example!
+vim.opt.mouse = "a"
+
+-- Don't show the mode, since it's already in the status line
+vim.opt.showmode = false
+
+-- Sync clipboard between OS and Neovim.
+--  Remove this option if you want your OS clipboard to remain independent.
+--  See `:help 'clipboard'`
+vim.opt.clipboard = "unnamedplus"
+
+vim.opt.confirm = true
+
+vim.opt.undofile = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.signcolumn = "yes"
+vim.opt.updatetime = 250
+-- Decrease mapped sequence wait time
+-- Displays which-key popup sooner
+vim.opt.timeoutlen = 300
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+-- Sets how neovim will display certain whitespace characters in the editor.
+--  See `:help 'list'`
+--  and `:help 'listchars'`
+vim.opt.list = true
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+
+-- Preview substitutions live, as you type!
+vim.opt.inccommand = "split"
+
+vim.opt.wrap = false
+vim.opt.showbreak = "↪"
+vim.opt.breakindent = true
+vim.opt.sidescrolloff = 5
+
+vim.opt.cursorline = true
+
+vim.opt.scrolloff = 5
+
+-- vim: ts=2 sts=2 sw=2 et
